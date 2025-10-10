be8dc0995dd889001bc5276ec14f8242750753ef
Scrappyz <ghostmic3000@gmail.com>
2025-10-10 10:40:01 +0800

    Added binaries for linux. Added binaries for spotify

---
+++ src/main/java/com/scrappyz/ytdlp/config/PathConfig.java
@@ -0,0 +1,10 @@
+package com.scrappyz.ytdlp.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties("path")
+public class PathConfig {
+    
+}
