82692693e97c0dcd3af23b18e1c56f7b585578bb
Scrappyz <ghostmic3000@gmail.com>
2025-10-13 21:10:17 +0800

    Added paste mechanic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,61 @@
-import React from 'react';
-import { MantineProvider, Button } from '@mantine/core';
+import React, { useState } from 'react';
+import { MantineProvider, Button, Flex, NativeSelect, TextInput, Group } from '@mantine/core';
 
 import { api } from './globals';
+import { useForm } from '@mantine/form';
+
+import '@mantine/core/styles.css';
 
 function App() {
-  const getData = async(): Promise<any> => {
+  // const getData = async(): Promise<any> => {
+  //   try {
+  //     const result = await fetch(api + "/downloads/HJGEOGH");
+  //     console.log(await result.json());
+  //   } catch(error) {
+  //     console.log(error);
+  //   }
+  // }
+
+  const [url, setUrl] = useState("");
+
+  const form = useForm({
+    mode: 'uncontrolled',
+    initialValues: {
+      type: "Video",
+      url: ""
+    }
+  });
+
+  const getClipboardText = async(): Promise<string> => {
     try {
-      const result = await fetch(api + "/downloads/HJGEOGH");
-      console.log(await result.json());
+      const clipText = await navigator.clipboard.readText();
+      return clipText;
     } catch(error) {
-      console.log(error);
+      return error;
     }
   }
 
+  const handlePaste = () => {
+    getClipboardText().then(res => {
+      console.log("Clipboard:", res);
+      form.setFieldValue("url", res);
+      setUrl(res);
+    })
+  }
+
   return (
-    <MantineProvider theme={{ colorScheme: 'dark' }}>
-      <Button onClick={() => getData()}>Panag</Button>
+    <MantineProvider defaultColorScheme="dark">
+      <Flex pl="10%" pr="10%" h="100vh" direction="row" justify="center" align="center">
+        <form onSubmit={form.onSubmit((values) => console.log(values))}>
+          <Flex direction="column" rowGap="lg">
+            <NativeSelect w="" key={form.key("type")} data={["Video", "Video Only", "Audio Only"]} />
+            <Group gap="0">
+              <TextInput key={form.key("url")} value={url} placeholder='Enter video link here' radius="0" onChange={(e) => setUrl(e.target.value)} />
+              <Button onClick={handlePaste} radius="0">Paste</Button>
+            </Group>
+          </Flex>
+        </form>
+      </Flex>
     </MantineProvider>
   );
 }
