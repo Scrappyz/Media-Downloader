835bc4178badbfb87db659f60da2d1a3352cd41b
Scrappyz <ghostmic3000@gmail.com>
2025-10-16 14:12:24 +0800

    added os detection

--- backend/src/main/java/com/scrappyz/ytdlp/config/PathProperties.java
+++ backend/src/main/java/com/scrappyz/ytdlp/config/PathProperties.java
@@ -5,6 +5,7 @@ import java.nio.file.Path;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
+import jakarta.annotation.PostConstruct;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,5 +17,21 @@ public class PathProperties {
     private Path binPath;
     private Path ytdlpBin;
     private Path downloadPath;
+
+    @PostConstruct
+    public void init() {
+        String bin = "";
+        String os = System.getProperty("os.name", "unknown").toLowerCase();
+
+        if(os.contains("win")) {
+            bin = "yt-dlp.exe";
+        } else if(os.contains("mac") || os.contains("darwin")) {
+            bin = "yt-dlp_mac";
+        } else {
+            bin = "yt-dlp_linux";
+        }
+
+        ytdlpBin = ytdlpBin.resolve(bin).normalize();
+    }
     
 }
