72daad2520ca43a2874d971a1b2bb2241f6b1e5d
Scrappyz <ghostmic3000@gmail.com>
2026-02-07 19:23:55 +0800

    added methods

--- backend/src/main/java/com/scrappyz/ytdlp/download/infrastructure/repository/RequestRepository.java
+++ backend/src/main/java/com/scrappyz/ytdlp/download/infrastructure/repository/RequestRepository.java
@@ -1,5 +1,18 @@
 package com.scrappyz.ytdlp.download.infrastructure.repository;
 
-public class RequestRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import com.scrappyz.ytdlp.download.infrastructure.entity.Request;
+
+public interface RequestRepository extends JpaRepository<Request, String> {
     
+    @Modifying
+    @Query("UPDATE Request r SET r.status = :status WHERE r.id = :requestId")
+    void updateStatusById(String requestId, String status);
+
+    @Modifying
+    @Query("UPDATE Request r SET r.completedAt = CURRENT_TIMESTAMP WHERE r.id = :requestId")
+    void updateCompletedAtById(String requestId);
 }
